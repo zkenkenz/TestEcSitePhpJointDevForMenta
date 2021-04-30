6f4e76db954142283c2dbea6841254ed5af29601
zkenkenz <kenji.dnt.11291225.kenjikenji@gmail.com>
2021-04-30 15:28:48 +0900

    css反映

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="storage/css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>@yield('title')</title>
 </head>
 
