98b385ea0d7d8255b28746800df3c699cf33e309
nobitaaaaa <web.nobita.net@gmail.com>
2021-05-13 18:02:06 +0900

    first commit

--- backend/app/Http/Controllers/ItemController.php
+++ backend/app/Http/Controllers/ItemController.php
@@ -26,9 +26,6 @@ class ItemController extends Controller
         //$items = Item::Paginate(6);
         $items = DB::table('items')->paginate(6);
 
-        DB::table('carts')
-            ->truncate();
-
         return view('items.index', compact('items'));
     }
 
@@ -66,50 +63,6 @@ class ItemController extends Controller
     }
 
 
-    public function postmycart(Request $request)
-    {
-        /**
-         * 
-         * 購入ボタンを押された時の処理
-         */
-
-        //カートボタンのpostの値をdbに保存、削除ボタンとの条件分け
-
-        if (isset($request->addCart)) {
-            $cart = new Cart;
-
-            $cart->item_id = $request->itemId;
-            $cart->name = $request->itemName;
-            if (isset($request->itemImg)) {
-                $cart->imgpash = $request->itemImg;
-            } else {
-                $cart->imgpash = "NULL.jpg";
-            }
-            $cart->detail = $request->itemDetail;
-            $cart->price = $request->itemPrice;
-            $cart->stock = $request->itemStock;
-            $cart->save();
-
-            //二重送信防止トークン
-            $request->session()->regenerateToken();
-            
-            $message = "商品を追加しました";
-        } elseif (isset($request->deleteCartItem)) {
-            $created_at = $request->created_at;
-            DB::table('carts')
-                ->where('created_at', $created_at)
-                ->delete();
-            $message = "商品を削除しました";
-        } elseif (isset($request->myCart)) {
-        }
-
-        if (!isset($message)) {
-            $message = "カートの中身";
-        }
-        $carts = DB::table('carts')->get();
-
-        return view('mycarts.index', compact('carts', 'message'));
-    }
 
 
     public function result(Request $request)
